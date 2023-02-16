1ec40800a6c1566b6c73f2c27c9013ab39ed5f96
RaghuveerChebrolu <raghuveerchebrolu@gmail.com>
2023-02-16 11:49:51 +0530

    push check

---
+++ src/test/java/com/AccessModifiers/accessModifier_Public.java
@@ -0,0 +1,17 @@
+package com.AccessModifiers;
+
+public class accessModifier_Public {
+	/*Note:
+	The public access modifier is accessible everywhere. It has the widest scope among all other modifiers.*/
+	
+	public int b = 9;
+	
+	public void msg12345() {
+		System.out.println("Hello inside public class and public method");
+	}
+	
+	public static void msg123456() {
+		System.out.println("Hello inside public class and public static method");
+	}
+
+}
